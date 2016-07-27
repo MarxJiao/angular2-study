80e64c95e5203de3de007efa69d38177d4eb9716
MarxJiao <610529111@qq.com>
2016-07-27 18:54:09 +0800

    webpack-quickstart runs up

--- webpack-quickstart/webpack.config.js
+++ webpack-quickstart/webpack.config.js
@@ -1,14 +1,28 @@
+var webpack = require('webpack');
 module.exports = {
-     entry: './app/main.ts',
-     output: {
-         path: './dist',
-         filename: 'app.bundle.js'
-     },
-     module: {
-         loaders: [{
-             test: /\.ts$/,
-             exclude: /node_modules/,
-             loader: 'ts-loader'
-         }]
-     }
- };
+    entry: {
+        app:'./app/main.ts',
+        vendor: './app/vendor.ts'
+    },
+    output: {
+        path: './dist',
+        filename: '[name].js'
+    },
+    resolve: {
+        extensions: ['', '.js', '.ts', '.css', '.html']
+    },
+    module: {
+        loaders: [{
+            test: /\.ts$/,
+            exclude: /node_modules/,
+            loaders: ['ts-loader']
+        }]
+    },
+    plugins: [
+        new webpack.optimize.CommonsChunkPlugin({
+            name: ['app', 'vendor']
+        }),
+        new webpack.optimize.UglifyJsPlugin(),
+    ]
+    
+}
